7b23c453867bb8da1804c43c944cbe953577d4c4
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2025-02-28 10:41:43 -0700

    [AGREGA] funciones anonimas y contactos

--- Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -8,27 +8,33 @@ import SwiftUI
 
 var contactos = [
     ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
-    ContactoAgenda(nombre: "Juan", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan2", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan3", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan4336534564", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan5", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan6", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan7", telefono: "12345"),
+    ContactoAgenda(nombre: "Juan8", telefono: "12345"),
 ]
 
 struct PantallaAgenda: View {
+    var largoDePantalla = UIScreen.main.bounds.width
+    var anchoDePantalla = UIScreen.main.bounds.height
+    
     var body: some View {
-        List{
-            VStack {
+        ScrollView{
+            VStack(spacing: 10) {
                 ForEach(contactos){ contacto in
-                    Text("\(contacto.nombre)")
-                    contacto_prevista()
+                    //Text("\(contacto.nombre)")
+                    contacto_prevista(contacroAMostrar: contacto, alPulsar: {print("Te envia saludos \(contacto.nombre) desde la pantalla agenda")})
                 }
                 
             }
             .background(Color.cyan)
+            .frame(width: largoDePantalla, alignment: Alignment/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
         }
+        .background(Color.green)
         
         
     }
--- Agenda Shufle/Agenda Shufle/componentes/contacto_prevista.swift
+++ Agenda Shufle/Agenda Shufle/componentes/contacto_prevista.swift
@@ -7,16 +7,46 @@
 
 import SwiftUI
 
+let contactoPruebas = ContactoAgenda(nombre: "Juan4336534564", telefono: "12345")
+
+
 struct contacto_prevista: View {
+    var contacroAMostrar: ContactoAgenda
+    var alPulsar: () -> Void = {print("hol")} //guarda una funcion sin pase de parametros y que devuelva nada
+    
+    let esquinaRedondeada = CGSize(width: 25, height: 25)
+    
     var body: some View {
-        HStack{
-            Image(systemName: "globe")
-            Text("Hola")
-            Text("Causa")
+        HStack(alignment: VerticalAlignment.center){
+            Spacer()
+            VStack{
+                Image("nombre_imagen")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 45, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerSize: esquinaRedondeada))
+                    .background(Color.blue)
+            }.background(Color.white)
+            
+            Spacer()
+                    
+            VStack(alignment: HorizontalAlignment.leading){//Alineamiento a la izquierda
+                Text(contacroAMostrar.nombre)
+                Text(contacroAMostrar.telefono)
+            }.background(Color.gray)
+                .frame(width: 150)
+            Spacer()
+            
         }
+        .frame(width: 250)
+        .background(Color.red)
+        .onTapGesture {
+            alPulsar()
+        }
+        
     }
 }
 
 #Preview {
-    contacto_prevista()
+    contacto_prevista(contacroAMostrar: contactoPruebas)
 }
